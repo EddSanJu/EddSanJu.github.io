85f20c553e5ee6b94e20af4dae1a00f81db6e34b
EddSJ <san21086@byui.edu>
2022-03-19 01:55:20 -0600

    created a form page

--- weather/js/js-two.js
+++ weather/js/js-two.js
@@ -41,3 +41,18 @@ function restoreClass () {
     }
 }
 
+function adjustRating(rating) {
+    document.getElementById("ratingvalue").innerHTML = rating;
+}
+
+function selectResponse() {
+	const s = document.querySelector('#selected')
+	const sel = document.querySelector('#selectbrowser');
+	s.style.display = "block";
+	s.textContent = sel.value;
+	
+}
+
+function redirect () {
+    window.location.href = "thanks.html";
+}
